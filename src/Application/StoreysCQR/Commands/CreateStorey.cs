35eb8bb0379d7b89913b736b3b3ca484eff118a4
Rene Havelka <havelka@outlook.sk>
2022-12-22 12:44:17 +0100

    Storey name - exceptions

--- src/Application/StoreysCQR/Commands/CreateStorey.cs
+++ src/Application/StoreysCQR/Commands/CreateStorey.cs
@@ -19,19 +19,8 @@ namespace Application.StoreysCQR.Commands
 
 		public async Task AddStorey(StoreysDto storeyDto)
 		{
-			//Exceptions - the name of the storey must
-			//- not be null or empty.
-			if (storeyDto.Name == null || storeyDto.Name == String.Empty) throw new ArgumentException($"Storey name must not be null or empty.");
-			//- consist of 4 characters at least.
-			if (storeyDto.Name.Length < 4) throw new ArgumentException("Storey name must consist of 4 characters at least.");
-			//- beginn with a capital letter.
-			var firstChar = storeyDto.Name[0];
-			if (!char.IsLetter(firstChar) || !char.IsUpper(firstChar)) throw new ArgumentException("The name of the storey must beginn with a capital letter.");
-			//- cosist of the maximum of 20 characters.
-			if (storeyDto.Name.Length > 20) throw new ArgumentException("Storey name must consist of the maximum of 20 characters.");
-			//- be distinct from the others.
-			var sameNames = _context.Storeys.Select(x => x.Name).Where(x => x.Equals(storeyDto.Name));
-			if (sameNames.Any()) throw new ArgumentException($"The name must be different, {storeyDto.Name} already exists.");
+			var storeyNameValidation = new StoreyNameValidation();
+			storeyNameValidation.StoreyNameValidate(storeyDto, _context);
 
 			Storeys storey = _mapper.Map<Storeys>(storeyDto);
 			await _context.Storeys.AddAsync(storey);
---
+++ src/Application/StoreysCQR/Commands/StoreyNameValidation.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.StoreysCQR.Commands
+{
+	public class StoreyNameValidation
+	{
+		public void StoreyNameValidate(StoreysDto storeyDto, IApplicationDbContext context)
+		{
+			//Exceptions - the name of the storey must
+			//- not be null or empty.
+			if (storeyDto.Name == null || storeyDto.Name == String.Empty) throw new ArgumentException($"Storey name must not be null or empty.");
+			//- consist of 4 characters at least.
+			if (storeyDto.Name.Length < 4) throw new ArgumentException("Storey name must consist of 4 characters at least.");
+			//- beginn with a capital letter.
+			var firstChar = storeyDto.Name[0];
+			if (!char.IsLetter(firstChar) || !char.IsUpper(firstChar)) throw new ArgumentException("Storey name must beginn with a capital letter.");
+			//- cosist of the maximum of 20 characters.
+			if (storeyDto.Name.Length > 20) throw new ArgumentException("Storey name must consist of the maximum of 20 characters.");
+			//- be distinct from the others.
+			var sameNames = context.Storeys.Select(x => x.Name).Where(x => x.Equals(storeyDto.Name));
+			if (sameNames.Any()) throw new ArgumentException($"The name must be different, {storeyDto.Name} already exists.");
+		}
+	}
+}
